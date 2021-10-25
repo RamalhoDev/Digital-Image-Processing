af83761e5110166da9748543cefd0674a4143433
Bruno Passeti <32283944+brunopasseti@users.noreply.github.com>
2021-10-24 22:28:48 -0300

    Update prewitt_filter.py

--- filters/prewitt_filter.py
+++ filters/prewitt_filter.py
@@ -5,7 +5,7 @@ import numpy as np
 class PrewittFilter(Filter):
 
 	def evaluate_pixel_rgb(self, indexes, line, column):
-		# r_horizontal = np.multiply(self.image[indexes['begin_line']:indexes['end_line']+1,indexes['begin_column']:indexes['end_column']+1, 0], self.mask[:,:,0]).sum()
+		r_horizontal = np.multiply(self.image[indexes['begin_line']:indexes['end_line']+1,indexes['begin_column']:indexes['end_column']+1, 0], self.mask[:,:,0]).sum()
 		r_vertical = np.multiply(self.image[indexes['begin_line']:indexes['end_line']+1,indexes['begin_column']:indexes['end_column']+1, 0], self.mask[:,:,1]).sum()
 
 		g_horizontal = np.multiply(self.image[indexes['begin_line']:indexes['end_line']+1,indexes['begin_column']:indexes['end_column']+1, 1], self.mask[:,:,0]).sum()
