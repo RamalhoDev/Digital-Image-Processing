c4f26605b3a4c9af54ca166966337f6f2c4a38e8
brunopasseti <>
2021-10-25 02:56:23 +0000

    Adding mask parser.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from filters.mean_filter import MeanFilter, MeanFilterY
 from filters.median_filter import MedianFilter, MedianFilterY
 from filters.prewitt_filter import PrewittFilter
 import numpy as np
+import sys
 
 def from_rgb_to_yiq(rgb: np.ndarray):
     yiq = np.ndarray(np.shape(rgb))
@@ -29,15 +30,48 @@ def from_yiq_to_rgb(yiq: np.ndarray):
 
     return rgb
 
-def read_filter_from_file(file):
+def read_mask_from_file(file):
+    filter = None
     with open(file, mode='r') as f:
-        for line in f:
-            arguments = [line for line in line.strip().split(' ')]
-            m = int(arguments[1])
-            n = int(arguments[2])
-            filter = np.zeros((m, n))
+        line = f.read().split(" ")
+        mode = line[0]
+        m = int(line[1])
+        n = int(line[2])
+        filter = np.zeros((m, n))
+        if mode == "mean":
             filter = filter + (1.0/(m * n))
-            return filter
+        elif mode == "emboss":
+            if n==3:
+                filter = np.array(((-2,-1,0),(-1,1,1),(0,1,2)))
+            elif n==5:
+                filter = np.array((-2,0,-1,0,0),(0,-2,-1,0,0),(-1,-1,1,1,1),(0,0,1,2,0),(0,0,1,0,2))
+            else:
+                assert False, f"{m} x {n} emboss filter not implemented yet"
+        elif mode == "prewitt":
+            if n==3:
+                filter = np.zeros((m,n,2))
+
+                filter[0,0,0] = 1
+                filter[0,1,0] = 1
+                filter[0,2,0] = 1
+                filter[2,0,0] = -1
+                filter[2,1,0] = -1
+                filter[2,2,0] = -1
+
+                filter[0,0,1] = 1
+                filter[1,0,1] = 1
+                filter[2,0,1] = 1
+                filter[0,2,1] = -1
+                filter[1,2,1] = -1
+                filter[2,2,1] = -1
+
+            else:
+                assert False, f"{m} x {n} prewitt filter not implemented yet"
+        elif mode == "median":
+            filter = np.ones((m, n))
+        else:
+            assert False, f"{m} x {n} {mode} filter not implemented yet (maybe a typo in {mode}?) modes supported:\nprewitt, emboss, mean, median\n"
+    return filter
 
 # def histogram_stretching(image : np.ndarray):
 #     min_value = image[:,:,0].min()
@@ -70,40 +104,17 @@ image = Image.open("Trabalhos-20211014/Woman.png").convert("RGB")
 
 pixels = np.array(image)
 
-mask = read_filter_from_file("teste.txt")
+mask = read_mask_from_file("masks/mean_3x3.txt")
+print("Using mask: ", mask)
 
-mask = np.zeros((3,3,2))
-
-mask[2,:,0] = -1
-mask[0,:,0] = 1
-mask[:,0,1] = 1
-mask[:,2,1] = -1
-
-
-# mask = np.zeros((5,5))
-# mask[0,0] = -2
-# mask[0,2] = -1
-# mask[1,1] = -2
-# mask[1,2] = -1
-# mask[2,0] = -1
-# mask[2,1] = -1
-# mask[2,2] = 1
-# mask[2,3] = 1
-# mask[2,4] = 1
-# mask[3,2] = 1
-# mask[3,3] = 2
-# mask[4,2] = 1
-# mask[4,4] = 2
-
-
-filter = PrewittFilter(mask)
+filter = MeanFilter(mask)
 
 filter.set_image(pixels)
 
 image_after_filter = filter.apply_filter_on_image()
 
 # image_after_filter = from_rgb_to_yiq(image_after_filter)
-image_after_filter = histogram_stretching(image_after_filter)
+# image_after_filter = histogram_stretching(image_after_filter)
 # image_after_filter = from_yiq_to_rgb(image_after_filter)
 
 PIL_image = Image.fromarray(image_after_filter.astype(np.uint8))
